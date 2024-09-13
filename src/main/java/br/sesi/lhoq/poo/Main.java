0e2ed7822e648095851896d582d1016a10e52545
inotunderstandbro <168561063+inotunderstandbro@users.noreply.github.com>
2024-09-13 11:31:03 -0300

    associando valores a objetos

--- src/main/java/br/sesi/lhoq/poo/Main.java
+++ src/main/java/br/sesi/lhoq/poo/Main.java
@@ -1,10 +1,29 @@
 package br.sesi.lhoq.poo;
 
+import br.sesi.lhoq.poo.cabeca.Cabeca;
+import br.sesi.lhoq.poo.cabeca.Olho;
+
 public class Main {
     public static void main(String[] args) {
-        Pessoa pessoa1 = new Pessoa();
-        pessoa1.cabeca.zoio.corIris = "marrom";
 
+        Pessoa pessoa1 = new Pessoa();
+        Olho Olho1 = new Olho();
+        Olho1.cego = true;
+        Olho1.diametroOlho = 2.0f;
+        Olho1.corIris = "azul";
+
+        Olho Olho2 = new Olho();
+        Olho2.cego = true;
+        Olho2.diametroOlho = 2.0f;
+        Olho2.corIris = "castanho";
+
+        Cabeca cabeca = new Cabeca();
+        cabeca.olhoEsquerdo = Olho1;
+        cabeca.olhoDireito = Olho2;
+
+
+
+        pessoa1.cabeca = cabeca;
 
 
     }
--- src/main/java/br/sesi/lhoq/poo/cabeca/Cabeca.java
+++ src/main/java/br/sesi/lhoq/poo/cabeca/Cabeca.java
@@ -1,13 +1,15 @@
 package br.sesi.lhoq.poo.cabeca;
 
 public class Cabeca {
-    public Olho zoio;
+    public Olho olhoEsquerdo;
+    public Olho olhoDireito;
    public Orelha zurea;
    public Nariz aspirador;
    public Boca bocudo;
 
    public Cabeca(){
-       this.zoio = new Olho();
+       this.olhoDireito = new Olho();
+       this.olhoEsquerdo = new Olho();
        this.zurea = new Orelha();
        this.aspirador = new Nariz();
        this.bocudo = new Boca();
